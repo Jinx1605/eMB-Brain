6de50dc3824475608c8d705d04427e5fb58930f5
Jinx1605 <34116980+Jinx1605@users.noreply.github.com>
2018-09-05 15:37:27 -0400

    gps updates

--- eMB-Brains-v5.ino
+++ eMB-Brains-v5.ino
@@ -17,6 +17,12 @@
 #include "OakOLED.h"
 OakOLED display;
 
+#include <SparkFun_I2C_GPS_Arduino_Library.h>
+I2CGPS myI2CGPS;
+
+#include <TinyGPS++.h> //From: https://github.com/mikalhart/TinyGPSPlus
+TinyGPSPlus gps; //Declare gps object
+
 #define TCAADDR 0x70
 
 struct MotorInfo {
@@ -29,17 +35,41 @@ struct MotorInfo {
 MotorInfo left_motor  = {"Left Motor" , 7, 00.00, 00.00};
 MotorInfo right_motor = {"Right Motor", 1, 00.00, 00.00};
 
+
+struct GPSData {
+  String time;
+  String date;
+  double lat;
+  double lng;
+};
+
+GPSData gData = {
+  "",   // time
+  "",   // date
+  0.00, // lat
+  0.00  // long
+};
+
 String format_time(int hr, int mn, int sx, int tzd){
   String theTime = "";
   String hur  = ((hr + tzd) > 12 ) ? String((hr + tzd) - 12 ) : String(hr + tzd);
   String ampm = ((hr + tzd) > 12 ) ? "pm" : "am";
   String min  = (mn < 10) ? "0" + String(mn) : String(mn);
   String sec  = (sx < 10) ? "0" + String(sx) : String(sx);
-  theTime += hur;
-  theTime += min;
+  theTime += hur + ":";
+  theTime += min + ":";
   theTime += sec;
   theTime += " " + ampm;
   return theTime;
+}
+
+String format_date(int mn, int dy, int yr) {
+  String theDate = "";
+  String month = (mn < 10) ? "0" + String(mn) : String(mn);
+  String day   = (dy < 10) ? "0" + String(dy) : String(dy);
+  String year  = String((yr - 2000));
+  theDate += month + "-" + day + "-" + year;
+  return theDate;
 }
 
 void tcaselect(uint8_t i) {
@@ -56,15 +86,51 @@ void setup() {
 
   init_motor(left_motor);
   init_motor(right_motor);
+
+  if (!myI2CGPS.begin()) {
+    oled_print(left_motor, "GPS Module");
+    oled_print(right_motor, "Failed Init");
+    Serial.println("Module failed to respond. Please check wiring.");
+    while (1); //Freeze!
+  } else {
+    oled_print(left_motor, "GPS Module");
+    oled_print(right_motor, "Working!");
+    Serial.println("GPS module found!");
+  }
   
 }
 
 void loop() {
 
+  while (myI2CGPS.available()) {
+    gps.encode(myI2CGPS.read()); //Feed the GPS parser
+  }
+
   collect_motor_temps(left_motor);
   collect_motor_temps(right_motor);
-  delay(250);
   
+  //Check to see if new GPS info is available
+  if (gps.time.isUpdated()) {
+    // displayInfo();
+    process_gps(gData);
+  }
+  
+}
+
+
+void process_gps(GPSData data){
+  data.time = format_time(gps.time.hour(),gps.time.minute(),gps.time.second(),-4);
+  data.date = format_date(gps.date.month(), gps.date.day(), gps.date.year());
+  data.lat  = gps.location.lat();
+  data.lng  = gps.location.lng(); 
+  
+  Serial.print(data.time + ",");
+  Serial.print(data.date + ",");
+  Serial.print(data.lat, 6);
+  Serial.print(",");
+  Serial.print(data.lng, 6);
+  Serial.print(",");
+  Serial.println();
 }
 
 void init_motor (MotorInfo side) {
@@ -110,13 +176,21 @@ void collect_motor_temps (MotorInfo side) {
 void display_motor_temps(MotorInfo side) {
   Serial.print(side.name + " Ambient = "); Serial.print(side.ambient); 
   Serial.print("*F\t" + side.name +" = "); Serial.print(side.object); Serial.println("*F");
-  Serial.println();
+  //Serial.println();
   
   display.clearDisplay();
   display.setCursor(0,0);
   display.print("A:" + String(side.ambient) + " F");
   display.setCursor(0,16);
   display.println("M:" + String(side.object) + " F");
+  display.display();
+}
+
+void oled_print(MotorInfo side, String txt) {
+  tcaselect(side.tcaport);
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print(txt);
   display.display();
 }
 
